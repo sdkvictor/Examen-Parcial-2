9bd6c5721ac6bbbb94c0e52fc03d2aace9d01069
Victor <sdkvictor@github.com>
2020-01-29 08:48:51 -0600

    parte 2 y 3

--- Parte-2/index.js
+++ Parte-2/index.js
@@ -3,12 +3,69 @@ let bodyParser = require( 'body-parser' );
 let mongoose = require( 'mongoose' );
 let jsonParser = bodyParser.json();
 let { DATABASE_URL, PORT } = require( './config' );
-
+let {BookmarkController} = require('./model');
+let uuid = require( 'uuid' );
 let app = express();
 
 let server;
 
 /* Tu código va aquí */
+let bookmark = {
+	id: uuid.v4(),
+	titulo: String,
+	descripcion: String,
+	url: String
+};
+
+app.put('/api/bookmarks/:id', jsonParser, (req,res)=>{
+	let id = req.body.id;
+	let paramsId = req.params.id;
+	let titulo= req.body.titulo;
+	let descripcion = req.body.descripcion;
+	let url= req.body.url;
+	if(id==undefined){
+		res.statusMessage = "id no es enviado en el cuerpo";
+		return status(406).send();
+	}
+	else if(paramsId!=id){
+		res.statusMessage = "ids no coinciden";
+		return status(409).send();
+	}
+	else if(titulo==undefined&&descripcion==undefined&&url==undefined){
+		res.statusMessage = "no se han proporcionado datos a actualizar";
+		return status(406).send();
+	}
+	else{
+		let newBookmark={};
+		if (titulo != undefined) {
+			newBookmark.titulo = titulo;
+		}
+		if (descripcion != undefined) {
+			newBookmark.descripcion = descripcion;
+		}
+		if (url != undefined) {
+			newBookmark.url = url;
+		}
+		BookmarkController.getById(id)
+        .then(bk => {
+            BookmarkController.update(id, newBookmark)
+                .then(b => {
+                    return res.status(202).json(b);
+                })
+                .catch(err => {
+					res.statusMessage = "Database error";
+					console.log(err);
+                    return res.status(501).send();
+                });
+        })
+        .catch(error => {
+            res.statusMessage = "Error al localizar el bookmark";
+            return res.status(404).send();
+        });
+	}
+
+});
+
 
 function runServer( port, databaseUrl ){
 	return new Promise( (resolve, reject ) => {
--- Parte-2/model.js
+++ Parte-2/model.js
@@ -4,7 +4,36 @@ let uuid = require( 'uuid' );
 mongoose.Promise = global.Promise;
 
 /* Tu código va aquí */
+let bookmarks = mongoose.Schema({
+    //id: uuid.v4(),
+    titulo: String,
+    descripcion: String,
+    url: String
+});
+
+let Bookmark = mongoose.model('bookmark', bookmarks);
+
+let BookmarkController = {
+    update: function(newBookmark){
+        return Bookmark.findByIdAndUpdate(id,newBookmark)
+        .then(newBookmark=>{
+            return newBookmark;
+        })
+        .catch(error=>{
+            throw Error(error);
+        });
+    },
+    getById: function(id){
+        return Bookmark.findById(id)
+        .then(bookmark=>{
+            return bookmark;
+        })
+        .catch(error=>{
+            throw Error(error);
+        });
+    }
+}
 
 module.exports = {
-    
+    BookmarkController
 };
